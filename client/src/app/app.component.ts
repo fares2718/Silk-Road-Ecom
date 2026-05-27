ba28f6567bc1421a997ce34b8dce75b81e1e7138
fares2718 <faresobaid2715@gmail.com>
2026-05-27 16:26:31 +0300

    feat(nav-bar): add responsive navigation bar with dropdown and search functionality
    - Implemented HTML structure for the navigation bar including logo, links, search form, and user profile dropdown.
    - Styled the navigation bar using SCSS for a modern look with responsive design.
    - Added TypeScript logic to handle dropdown visibility for user profile options.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { NavBarComponent } from './core/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [NavBarComponent]
 })
 export class AppComponent {
   title = 'client';
---
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.scss'
+})
+export class NavBarComponent {
+
+isDropDownVisible = false;
+toggleDorpDown(){
+  this.isDropDownVisible = !this.isDropDownVisible;
+}
+
+}
