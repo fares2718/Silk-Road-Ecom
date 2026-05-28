2da1c9d918deeec6f96a5b697e60200609439c7f
fares2718 <faresobaid2715@gmail.com>
2026-05-28 16:33:42 +0300

    feat: add home and product details components with styles and HTML structure
    - Implemented HomeComponent with hero section and features section.
    - Created corresponding SCSS for styling the home component.
    - Added ProductDetailsComponent with basic HTML structure.
    - Included empty SCSS file for product details styling.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from './components/core/nav-bar/nav-bar.component';
-import { ShopComponent } from "./components/shop/shop.component";
 import { FooterComponent } from './components/core/footer/footer.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [NavBarComponent, ShopComponent,FooterComponent]
+  imports: [NavBarComponent, FooterComponent, RouterOutlet]
 })
 export class AppComponent {
   title = 'client';
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ShopComponent } from './components/shop/shop.component';
+import { HomeComponent } from './components/core/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  
+  { path: 'shopping', component: ShopComponent },
+
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+  { path: '**', redirectTo: 'home' },
+];
