5f15b5e869dc09411ddee54556ca49a001b682ee
fares2718 <faresobaid2715@gmail.com>
2026-05-28 15:08:29 +0300

    feat(footer): add footer component with branding, quick links, customer service, and newsletter subscription

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { NavBarComponent } from './components/core/nav-bar/nav-bar.component';
 import { ShopComponent } from "./components/shop/shop.component";
+import { FooterComponent } from './components/core/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [NavBarComponent, ShopComponent]
+  imports: [NavBarComponent, ShopComponent,FooterComponent]
 })
 export class AppComponent {
   title = 'client';
