ed3b223cba681341431e12e59d8c153ce2316ddb
fares2718 <faresobaid2715@gmail.com>
2026-05-30 10:56:03 +0300

    feat:add Basket Controller

---
+++ src/SilkRoad.API/Controllers/BasketController.cs
@@ -0,0 +1,59 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SilkRoad.API;
+using SilkRoad.API.Controllers;
+using SilkRoad.Core;
+
+namespace MyApp.Namespace
+{
+    public class BasketController : BaseController
+    {
+        public BasketController(IUnitOfWork uow, IMapper mapper) : base(uow, mapper)
+        {
+        }
+
+        [HttpPost("add-update-basket")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> AddUpdateBasketAsync(CustomerBasket Basket)
+        {
+            if(Basket is null)
+                return BadRequest(new APIResponse(400));
+            CustomerBasket? basket = await _uow.CustomerBasketRepository.AddUpdateBasketAsync(Basket);
+            if(basket is null)
+                return NotFound(new APIResponse(404));
+            return Ok(basket);
+        }
+
+        [HttpPost("delete-basket/{Id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> DeleteBasketAsync(string Id)
+        {
+            if(string.IsNullOrEmpty(Id))
+                return BadRequest(new APIResponse(400));
+            bool isDeleted = await _uow.CustomerBasketRepository.DeleteBasketAsync(Id);
+            return isDeleted? Ok(new APIResponse(200))
+                : NotFound(new APIResponse(404));
+        }
+
+
+        [HttpGet("basket/{Id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+
+        public async Task<IActionResult> GetBasketByIdAsync(string Id)
+        {
+            if(string.IsNullOrEmpty(Id))
+                return BadRequest(new APIResponse(400));
+            CustomerBasket? basket = await _uow.CustomerBasketRepository.GetBasketByIdAsync(Id);
+            if(basket is null)
+                return NotFound(new APIResponse(404));
+            return Ok(basket);
+        }
+    }
+}
--- src/SilkRoad.Core/Entities/Basket/BasketItem.cs
+++ src/SilkRoad.Core/Entities/Basket/BasketItem.cs
@@ -5,6 +5,7 @@ public class BasketItem
     public int ItemID { get; set; }
     public string ItemName { get; set; } = string.Empty;
     public string Category { get; set; } = string.Empty;
+    public string ImageURL { get; set; } = string.Empty;
     public int Quantity { get; set; }
     public decimal Price { get; set; }
 
