0d06202d23a057d567d54dbefda472207ead6077
fares2718 <faresobaid2715@gmail.com>
2026-05-30 09:51:00 +0300

    feat: add BasketItem and CustomerBasket classes for shopping cart functionality

---
+++ src/SilkRoad.Core/Entities/Basket/BasketItem.cs
@@ -0,0 +1,11 @@
+﻿namespace SilkRoad.Core;
+
+public class BasketItem
+{
+    public int ItemId { get; set; }
+    public string ItemName { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+
+}
---
+++ src/SilkRoad.Core/Entities/Basket/CustomerBasket.cs
@@ -0,0 +1,13 @@
+﻿namespace SilkRoad.Core;
+
+public class CustomerBasket
+{
+    public CustomerBasket() { }
+
+    public CustomerBasket(int basketId)
+    {
+        BasketId = basketId;
+    }
+    public int BasketId { get; set; }
+    public List<BasketItem> BasketItems { get; set; } = new List<BasketItem>();
+}
