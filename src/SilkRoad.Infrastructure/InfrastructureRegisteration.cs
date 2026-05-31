edc5f44539e2d46928a13ba9ce34a86ab0c47262
fares2718 <faresobaid2715@gmail.com>
2026-05-31 17:21:55 +0300

    registered authintecation

--- src/SilkRoad.Infrastructure/InfrastructureRegisteration.cs
+++ src/SilkRoad.Infrastructure/InfrastructureRegisteration.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using System.Text;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.IdentityModel.Tokens;
 using SilkRoad.Core;
 using SilkRoad.Core.Services;
 using StackExchange.Redis;
@@ -11,7 +16,7 @@ namespace SilkRoad.Infrastructure;
 
 public static class InfrastructureRegisteration
 {
-    public static IServiceCollection InfrastructureConfiguration(this IServiceCollection services 
+    public static IServiceCollection InfrastructureConfiguration(this IServiceCollection services
     , IConfiguration configuration)
     {
         services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
@@ -27,20 +32,62 @@ public static class InfrastructureRegisteration
             return ConnectionMultiplexer.Connect(config);
         });
 
-        services.AddScoped<IEmailService,EmailService>();
-        services.AddScoped<IGenerateToken,GenerateToken>();
+        services.AddScoped<IEmailService, EmailService>();
+        services.AddScoped<IGenerateToken, GenerateToken>();
 
-        services.AddDbContext<AppDbContext>(options =>{
+        services.AddDbContext<AppDbContext>(options =>
+        {
             options.UseSqlServer(configuration.GetConnectionString("SilkRoadCon"));
         });
 
         services.AddIdentityCore<AppUser>(options =>
         {
-           options.Password.RequireDigit = true; 
+            options.Password.RequireDigit = true;
         })
         .AddRoles<IdentityRole>()
         .AddEntityFrameworkStores<AppDbContext>();
 
+        services.AddAuthentication(confOp =>
+       {
+           confOp.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+           confOp.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+           confOp.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+       })
+       .AddCookie(confOp =>
+       {
+           confOp.Cookie.Name = "token";
+           confOp.Events.OnRedirectToLogin = context =>
+           {
+               context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+               return Task.CompletedTask;
+           };
+       })
+       .AddJwtBearer(confOp =>
+       {
+           confOp.RequireHttpsMetadata = false;
+           confOp.SaveToken = true;
+           confOp.TokenValidationParameters = new TokenValidationParameters
+           {
+               ValidateIssuer = true,
+               ValidateAudience = true,
+               ValidateLifetime = true,
+               ValidateIssuerSigningKey = true,
+               ValidIssuer = configuration["JWT:Issuer"],
+               ValidAudience = configuration["JWT:Audience"],
+               IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(configuration["JWT_SECRET_KEY"]!))
+           };
+           confOp.Events = new JwtBearerEvents
+           {
+
+               OnMessageReceived = context =>
+               {
+                   var token = context.Request.Cookies["token"];
+                   context.Token = token;
+                   return Task.CompletedTask;
+               }
+           };
+       });
         return services;
     }
 }
