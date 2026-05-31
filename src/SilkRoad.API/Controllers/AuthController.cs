69ee203fda496b0f8e6904ae20d6c2d6ab8e04be
fares2718 <faresobaid2715@gmail.com>
2026-05-31 18:57:27 +0300

    Add login endpoint

--- src/SilkRoad.API/Controllers/AuthController.cs
+++ src/SilkRoad.API/Controllers/AuthController.cs
@@ -11,8 +11,31 @@ public class AuthController : BaseController
     {
     }
 
+    [HttpPost("login")]
+
+    public async Task<IActionResult> Login(LoginDTO loginDTO)
+    {
+        var result = await _uow.Auth.LoginAsync(loginDTO);
+        if(result is null)
+            return BadRequest(new APIResponse(400));
+        if(result.Contains("aren't registered"))
+            return NotFound(new APIResponse(404,result));
+        if(result.StartsWith("Please"))
+            return Unauthorized(new APIResponse(401,result));
+        Response.Cookies.Append("token",result!,new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = true,
+            SameSite = SameSiteMode.None,
+            IsEssential = true,
+            Domain = "localhost",
+            Expires = DateTime.Now.AddDays(1)
+        });
+        return Ok(new APIResponse(200));
+    }
+
     [HttpPost("register")]
-    public async Task<IActionResult> RegisterAsync(RegisterDTO registerDTO)
+    public async Task<IActionResult> Register(RegisterDTO registerDTO)
     {
         var result = await _uow.Auth.RegisterAsync(registerDTO);
             
